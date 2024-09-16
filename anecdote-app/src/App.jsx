4d3636c4430cfde2066fa1a2c7833a747f7122f7
Daria Zakharova <t3zada00@students.oamk.fi>
2024-09-16 10:27:50 +0300

    Create useField custom hook for anecdote creation form (ex. 7.4-7.6)

--- anecdote-app/src/App.jsx
+++ anecdote-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import {
   Routes, Route, Link, useMatch, useNavigate
 } from 'react-router-dom'
+import  { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -61,27 +62,33 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const { reset: resetContent, ...contentProps } = useField('text')
+  const { reset: resetAuthor, ...authorProps } = useField('text')
+  const { reset: resetInfo, ...infoProps } = useField('text')
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
 
-    props.setNotification(`anecdote '${content}' created!`)
+    props.setNotification(`anecdote '${content.value}' created!`)
     setTimeout(() => {
       props.setNotification('')
     }, 5000)
 
     navigate('/')
+  }
+
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -90,17 +97,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...contentProps} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...authorProps} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...infoProps} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
